d0cd0527436158adac7ef1ac5c22c9da3d831edc
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2020-07-21 18:20:42 +0700

    up

--- js/menikah.js
+++ js/menikah.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
       $el.addEventListener("click", function() {
         // Get the target from the "data-target" attribute
         var target = $el.dataset.target;
-        var $target = document.getElementById(target);
+        var $target = document.getElementById(target); 
         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
         $el.classList.toggle("is-active");
         $target.classList.toggle("is-active");
